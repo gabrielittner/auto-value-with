000fed16785b3f904ace128675c0c321f7a1e561
Gabriel Ittner <gabriel.ittner@gmail.com>
2016-05-13 12:31:44 +0200

    AutoValue 1.3 preparation: don't do name and type checks in applicable() and consumeProperties()

--- auto-value-with/src/main/java/com/gabrielittner/auto/value/with/AutoValueWithExtension.java
+++ auto-value-with/src/main/java/com/gabrielittner/auto/value/with/AutoValueWithExtension.java
@@ -1,7 +1,6 @@
 package com.gabrielittner.auto.value.with;
 
 import com.gabrielittner.auto.value.util.Property;
-import com.google.auto.common.MoreElements;
 import com.google.auto.service.AutoService;
 import com.google.auto.value.extension.AutoValueExtension;
 import com.google.common.collect.ImmutableList;
@@ -9,7 +8,6 @@ import com.google.common.collect.ImmutableSet;
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 import java.util.ArrayList;
@@ -18,102 +16,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.TypeMirror;
 
 import static com.gabrielittner.auto.value.util.AutoValueUtil.getAutoValueClassClassName;
 import static com.gabrielittner.auto.value.util.AutoValueUtil.newFinalClassConstructorCall;
 import static com.gabrielittner.auto.value.util.AutoValueUtil.newTypeSpecBuilder;
+import static com.gabrielittner.auto.value.with.WithMethod.filterMethods;
+import static com.gabrielittner.auto.value.with.WithMethod.getWithMethods;
 import static javax.lang.model.element.Modifier.FINAL;
 
 @AutoService(AutoValueExtension.class)
 public class AutoValueWithExtension extends AutoValueExtension {
 
-    private static final String PREFIX = "with";
-
-    private static List<WithMethod> getWithMethods(Context context) {
-        ProcessingEnvironment environment = context.processingEnvironment();
-        TypeElement autoValueClass = context.autoValueClass();
-        ImmutableSet<ExecutableElement> methods = MoreElements.getLocalAndInheritedMethods(
-                autoValueClass, environment.getElementUtils());
-        Map<String, ExecutableElement> propertyNames = context.properties();
-        List<WithMethod> withMethods = new ArrayList<>(methods.size());
-        for (ExecutableElement method : methods) {
-            String methodName = method.getSimpleName().toString();
-            if (!method.getModifiers().contains(Modifier.ABSTRACT)) {
-                continue;
-            }
-            if (!methodName.startsWith(PREFIX)) {
-                continue;
-            }
-            List<? extends VariableElement> parameters = method.getParameters();
-            if (parameters.size() == 0) {
-                continue;
-            }
-
-            Property property;
-            String propertyName = removePrefix(methodName);
-            ExecutableElement element = propertyNames.get(propertyName);
-            if (element != null) {
-                property = new Property(propertyName, element);
-            } else {
-                throw new IllegalArgumentException(String.format("%s doesn't have property with name"
-                        + " %s which is required for %s()", autoValueClass, propertyName, methodName));
-            }
-
-            if (parameters.size() != 1
-                    || !TypeName.get(parameters.get(0).asType()).equals(property.type())) {
-                throw new IllegalArgumentException(String.format("Expected single argument of type"
-                        + " %s for %s()", property.type(), methodName));
-            }
-            TypeMirror returnType = method.getReturnType();
-            if (!environment.getTypeUtils().isAssignable(autoValueClass.asType(), returnType)) {
-                throw new IllegalArgumentException(String.format("%s() in %s returns %s, expected %s",
-                        methodName, autoValueClass, returnType, autoValueClass));
-            }
-            withMethods.add(new WithMethod(methodName, method.getModifiers(),
-                    method.getAnnotationMirrors(), property));
-        }
-        return withMethods;
-    }
-
-    private static String removePrefix(String name) {
-        return Character.toLowerCase(name.charAt(PREFIX.length()))
-                + name.substring(PREFIX.length() + 1);
-    }
-
-    private static class WithMethod {
-        private final String methodName;
-        private final Set<Modifier> methodModifiers;
-        private final List<? extends AnnotationMirror> methodAnnotations;
-
-        private final Property property;
-
-        WithMethod(String methodName, Set<Modifier> methodModifiers,
-                List<? extends AnnotationMirror> methodAnnotations, Property property) {
-            this.methodName = methodName;
-            this.methodModifiers = methodModifiers;
-            this.methodAnnotations = methodAnnotations;
-            this.property = property;
-        }
-    }
-
     @Override
     public boolean applicable(Context context) {
-        return getWithMethods(context).size() > 0;
+        return filterMethods(context).size() > 0;
     }
 
     @Override
     public Set<String> consumeProperties(Context context) {
-        List<WithMethod> withMethods = getWithMethods(context);
-        Set<String> consumedProperties = new HashSet<>(withMethods.size());
-        for (WithMethod method : withMethods) {
-            consumedProperties.add(method.methodName);
+        //TODO AutoValue 1.3: use consumeMethods(Context) instead
+        ImmutableSet<ExecutableElement> methods = filterMethods(context);
+        Set<String> consumedProperties = new HashSet<>(methods.size());
+        for (ExecutableElement method : methods) {
+            consumedProperties.add(method.getSimpleName().toString());
         }
         return consumedProperties;
     }
