64d00455bf0c0cb625bd02a67e5ee1ae645ef33b
Gabriel Ittner <gabriel.ittner@gmail.com>
2018-11-04 16:31:04 +0100

    use getResolvedReturnType() from extension utils (#56)

--- auto-value-with/src/main/java/com/gabrielittner/auto/value/with/WithMethod.java
+++ auto-value-with/src/main/java/com/gabrielittner/auto/value/with/WithMethod.java
@@ -5,23 +5,20 @@ import com.google.auto.value.extension.AutoValueExtension.Context;
 import com.google.common.collect.ImmutableSet;
 import com.squareup.javapoet.TypeName;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.TypeParameterElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic.Kind;
+
+import static com.gabrielittner.auto.value.util.ElementUtil.getResolvedReturnType;
 
 class WithMethod {
 
@@ -97,77 +94,5 @@ class WithMethod {
             }
         }
         return withMethods.build();
-    }
-
-    static TypeMirror getResolvedReturnType(
-            Types typeUtils, TypeElement type, ExecutableElement method) {
-        TypeMirror returnType = method.getReturnType();
-        if (returnType.getKind() == TypeKind.TYPEVAR) {
-            List<HierarchyElement> hierarchy =
-                    getHierarchyUntilClassWithElement(typeUtils, type, method);
-            return resolveGenericType(hierarchy, returnType);
-        }
-        return returnType;
-    }
-
-    private static List<HierarchyElement> getHierarchyUntilClassWithElement(
-            Types typeUtils, TypeElement start, Element target) {
-
-        for (TypeMirror superType : typeUtils.directSupertypes(start.asType())) {
-            TypeElement superTypeElement = (TypeElement) typeUtils.asElement(superType);
-            if (superTypeElement.getEnclosedElements().contains(target)) {
-                HierarchyElement base = new HierarchyElement(superTypeElement, null);
-                HierarchyElement current = new HierarchyElement(start, superType);
-                return new ArrayList<>(Arrays.asList(base, current));
-            }
-        }
-
-        for (TypeMirror superType : typeUtils.directSupertypes(start.asType())) {
-            TypeElement superTypeElement = (TypeElement) typeUtils.asElement(superType);
-            List<HierarchyElement> result =
-                    getHierarchyUntilClassWithElement(typeUtils, superTypeElement, target);
-            if (result != null) {
-                result.add(new HierarchyElement(start, superType));
-                return result;
-            }
-        }
-        return null;
-    }
-
-    private static TypeMirror resolveGenericType(
-            List<HierarchyElement> hierarchy, TypeMirror type) {
-
-        int position = indexOfParameter(hierarchy.get(0).element, type.toString());
-        for (int i = 1; i < hierarchy.size(); i++) {
-            HierarchyElement hierarchyElement = hierarchy.get(i);
-
-            type = ((DeclaredType) hierarchyElement.superType).getTypeArguments().get(position);
-            if (type.getKind() != TypeKind.TYPEVAR) {
-                return type;
-            }
-
-            position = indexOfParameter(hierarchyElement.element, type.toString());
-        }
-        throw new IllegalArgumentException("Couldn't resolve type " + type);
-    }
-
-    private static class HierarchyElement {
-        private final TypeElement element;
-        private final TypeMirror superType;
-
-        private HierarchyElement(TypeElement element, TypeMirror superType) {
-            this.element = element;
-            this.superType = superType;
-        }
-    }
-
-    private static int indexOfParameter(TypeElement element, String param) {
-        List<? extends TypeParameterElement> params = element.getTypeParameters();
-        for (int i = 0; i < params.size(); i++) {
-            if (params.get(i).getSimpleName().toString().equals(param)) {
-                return i;
-            }
-        }
-        throw new IllegalArgumentException("Param " + param + "not not found in list");
     }
 }
